5a5f632bf9bbb638dc575e26ef299b9982972927
LudvigRisan <ludvig.risan@gmail.com>
2026-02-01 12:06:38 +0100

    More sceneries

--- SoftBody/Timer.gd
+++ SoftBody/Timer.gd
@@ -1,7 +1,11 @@
 extends Label3D
 
+var startTime: int
+
+func _ready() -> void:
+	startTime = Time.get_ticks_msec()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	@warning_ignore("integer_division")
-	text = var_to_str(Time.get_ticks_msec() / 1000)
+	text = var_to_str((Time.get_ticks_msec() - startTime) / 1000)
